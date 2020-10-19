7bc6ebc871ad684630b09edab004c49583dbc3cf
jimmyzip <jhkimjimmy@gmail.com>
2020-10-20 00:25:37 +0900

    promise chaining ver2

--- promiseExam/promiseChaining.js
+++ promiseExam/promiseChaining.js
@@ -2,11 +2,19 @@ var fs = require("fs");
 
 var data = ["김창수", "01077859923", 25000];
 
+//function 형태로 promise처리 후 리턴값을 promise타입으로 처리하는 버전
+function makeCsv(value) {
+  return new Promise( (resolve, reject) => {
+    resolve(value);
+  });
+}
+
 console.log("시작");
 
-new Promise((resolve, reject) => {
-  resolve(data[0]);
-}).then((result) => {
+//new Promise((resolve, reject) => {
+//  resolve(data[0]);
+//}).then((result) => {
+makeCsv(data[0]).then((result) => {
   console.log(result);  //김창수
   return result+","+data[1];
 }).then((result) => {
