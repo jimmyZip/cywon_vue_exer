1d9d325f7f22ccd1579d1c1b7f55af2753931e80
jimmyzip <jhkimjimmy@gmail.com>
2020-10-21 00:53:27 +0900

    axios with json server GET request example

--- axiosCdnWithJsonServer/app.js
+++ axiosCdnWithJsonServer/app.js
@@ -1,3 +1,16 @@
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    employees: []
+  },
+  created() {
+    axios.get('http://localhost:3000/employees')
+      .then((res) => {
+        console.log(res);//요청 성공
+        this.employees = res.data;
+      })
+      .catch((err) => {
+        console.log(err);//에러 발생 시
+      })
+  }
 })
