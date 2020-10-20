6d8de35e85b094eafbe096800a9b5b115012fe83
jimmyzip <jhkimjimmy@gmail.com>
2020-10-21 01:16:58 +0900

    axios with simple json server POST method example

--- axiosCdnWithJsonServer/app.js
+++ axiosCdnWithJsonServer/app.js
@@ -1,7 +1,12 @@
 new Vue({
   el: '#app',
   data: {
-    employees: []
+    employees: [],
+    //POST 메서드를 이용한 예제 용도로 employee 객체를 뷰 인스턴스의 data 객체에 추가
+    employee: { id: 0, name: '', email: ''},
+    isUpdate: false,
+    show: false,
+    send: false
   },
   created() {
     axios.get('http://localhost:3000/employees')
@@ -12,5 +17,36 @@ new Vue({
       .catch((err) => {
         console.log(err);//에러 발생 시
       })
+  },
+  methods: {
+    showForm(emp) {
+      this.show = true;
+    },
+    btnOk() {
+      this.send = true;
+      if(this.isUpdate == false) {
+        this.addEmployee();
+      }
+    },
+    btnCancel() {
+      this.show = false;
+      this.send = false;
+    },
+    addEmployee() {
+      if(this.send == true) {
+        axios.post('http://localhost:3000/employees',
+        {
+          "id": this.employee.id,
+          "name": this.employee.name,
+          "email": this.employee.email
+        }).then((res) => {
+          console.log(res);
+        }).catch((err) => {
+          console.log(err);
+        })
+        this.show = false;
+        this.send = false;
+      }
+    }
   }
 })
